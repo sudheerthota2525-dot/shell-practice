4daa6290cd8799a9811c4e302b57859a335279e8
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-09-28 22:50:13 -0400

    Conversation

---
+++ 2-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Kalyan:: Hi Sai, How are you?"
+echo "Sai:: Hello Kalyan, I am fine. How are you?"
+echo "Kalyan:: What are you learning recently?"
+echo "Sai:: I am learning Shell scripting Kalyan"
