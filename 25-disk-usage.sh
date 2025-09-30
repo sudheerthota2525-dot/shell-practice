64054084066d8b58078389bebe756fd224a0e521
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-09-30 16:47:41 -0400

    disk-usage

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
-DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=2  # in real project keep 75
+# Disk Usage Monitoring Script
+# Author: Sudheer / DevOps Team
+
+# Threshold (in %). In real project keep it 75
+DISK_THRESHOLD=75  
+
+# Get private IP of EC2 instance
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# Get disk usage (excluding header)
+DISK_USAGE=$(df -hT | grep -vE '^Filesystem')
+
 MESSAGE=""
 
+# Loop through each line of df output
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk '{print $6}' | tr -d '%')
     PARTITION=$(echo $line | awk '{print $7}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        MESSAGE+="High Disk usage on $PARTITION: $USAGE % <br>"
+
+    if [ "$USAGE" -ge "$DISK_THRESHOLD" ]; then
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
     fi
 done <<< "$DISK_USAGE"
 
+# Debug print
 echo -e "Message Body: $MESSAGE"
 
-sh mail.sh "shanmukhathota21@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+# If message is not empty, send mail
+if [ -n "$MESSAGE" ]; then
+    sh mail.sh "shanmukhathota21@gmail.com" \
+               "High Disk Usage Alert" \
+               "High Disk Usage" \
+               "$MESSAGE" \
+               "$IP_ADDRESS" \
+               "DevOps Team"
+fi
