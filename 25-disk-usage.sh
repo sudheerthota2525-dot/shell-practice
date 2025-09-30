cb0a616cfb416bc7d92fe1f01cd33d24d0bb7a2c
Thota Shanmukha Sudheer <sudheerthota2525@gmail.com>
2025-09-30 16:08:58 -0400

    template

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=2 # in project we keep it as 75
+DISK_THRESHOLD=2  # in real project keep 75
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 
@@ -10,16 +9,10 @@ do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        MESSAGE+="High Disk usage on $PARTITION: $USAGE % <br>" # escaping
+        MESSAGE+="High Disk usage on $PARTITION: $USAGE % <br>"
     fi
-done <<< $DISK_USAGE
+done <<< "$DISK_USAGE"
 
 echo -e "Message Body: $MESSAGE"
 
 sh mail.sh "shanmukhathota21@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
-
-# TO_ADDRESS=$1
-# SUBJECT=$2
-# ALERT_TYPE=$3
-# MESSAGE_BODY=$4
-# IP_ADDRESS=$5
--- mail.sh
+++ mail.sh
@@ -3,11 +3,15 @@ TO_ADDRESS=$1
 SUBJECT=$2
 ALERT_TYPE=$3
 MESSAGE_BODY=$4
-FORMATTED_BODY=$(printf '%s\n' "$MESSAGE_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
 IP_ADDRESS=$5
 TO_TEAM=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
+FORMATTED_BODY=$(printf '%s\n' "$MESSAGE_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
+
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" \
+                 -e "s/ALERT_TYPE/$ALERT_TYPE/g" \
+                 -e "s/IP_ADDRESS/$IP_ADDRESS/g" \
+                 -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
 
 {
 echo "To: $TO_ADDRESS"
